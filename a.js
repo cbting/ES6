72f6dcb6c3943085f6dc090cd71d663c9d24a491
陈冰婷 <chenbingting@chenbingtingdeMacBook-Air.local>
2019-07-15 15:57:09 +0800

    init

---
+++ a.js
@@ -0,0 +1,7 @@
+var firstName = 'Michael';
+var lastName = 'Jackson';
+var year = 1958;
+
+const obj = { firstName, lastName, year };
+
+export default obj
---
+++ test.js
@@ -0,0 +1,7 @@
+import { firstName } from './a.js';
+
+console.log(firstName);
+
+import element from '/Users/chenbingting/.nvm/versions/node/v8.16.0/lib/node_modules/element-ui';
+
+console.log(element);
