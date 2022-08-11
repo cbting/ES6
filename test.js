30704edf4f75a4ebb12a1c7e1269ac22be6a0598
cbtTest <111034139+cbtTest@users.noreply.github.com>
2022-08-11 11:20:32 +0800

    change test

--- test.js
+++ test.js
@@ -5,3 +5,5 @@ console.log(firstName);
 import element from '/Users/chenbingting/.nvm/versions/node/v8.16.0/lib/node_modules/element-ui';
 
 console.log(element);
+
+console.log("This is a 代码审查 test！！")
